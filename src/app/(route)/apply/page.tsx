0d06630842c8c1836e85cbaad63ee497141fcf14
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2025-03-10 16:32:39 +0900

    fix : 필요없는 코드 제거

--- src/app/(route)/apply/page.tsx
+++ src/app/(route)/apply/page.tsx
@@ -1,62 +1,7 @@
 "use client";
 
 const Apply = () => {
-  return (
-    <div>
-      {/* <Header />
-      <div className="flex justify-center my-[200px]">
-        <div className="flex flex-col w-full max-w-[600px]">
-          <Confirm />
-          <div className="mt-3 flex flex-col ">
-            <div className="mt-9">
-              <Label title="이름" require={true} />
-              <Input placeholder="이름을 입력해주세요." />
-            </div>
-            <div className="mt-9">
-              <Label title="연락처" require={true} />
-              <Input placeholder="연락처를 입력해주세요." />
-            </div>
-
-            <div className="mt-9">
-              <Label title="학년" />
-              <div className="mt-4 flex flex-wrap justify-between gap-4">
-                <SelectButton title="1학년" width="290px" />
-                <SelectButton title="2학년" width="290px" />
-                <SelectButton title="3학년" width="290px" />
-                <SelectButton title="4학년" width="290px" />
-              </div>
-            </div>
-
-            <div className="mt-9">
-              <Label title="재학" />
-              <div className="mt-4 flex flex-wrap justify-between gap-4">
-                <SelectButton title="재학" width="290px" />
-                <SelectButton title="휴학" width="290px" />
-              </div>
-            </div>
-            <div className="mt-9">
-              <Label title="학과" />
-              <div className="mt-4  flex flex-wrap justify-between gap-4">
-                <SelectButton title="컴퓨터공학부 소프트웨어전공" />
-                <SelectButton title="컴퓨터공학부 컴퓨터공학전공" />
-                <div className="flex w-full gap-3">
-                  <SelectButton title="기타" width="152px" />
-                  <Input placeholder="이름을 입력해주세요." margin="0px" />
-                </div>
-              </div>
-            </div>
-            <div className="mt-9 mb-12">
-              <Label title="학번" />
-              <Input placeholder="학번을 입력해주세요." />
-            </div>
-          </div>
-
-          <div></div>
-          <div></div>
-        </div>
-      </div> */}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Apply;
