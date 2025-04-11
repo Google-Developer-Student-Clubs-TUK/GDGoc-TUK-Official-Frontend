989e797838d23374a9e46ec90ffd54bd052e69bd
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2025-04-12 01:14:14 +0900

    setting :  gray color setting

--- src/app/(route)/memberManage/page.tsx
+++ src/app/(route)/memberManage/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React, { useState } from "react";
+import Member from "./_component/Member";
 
 const MemberManage = () => {
   const [memberTab, setMemberTap] = useState<"회원" | "지원자">("회원");
 
   return (
     <div className="my-[200px] mx-[100px] flex flex-col">
-      <h1 className="text-[28px] font-bold text-white">회원 관리</h1>
+      <h1 className="text-[28px] font-bold  text-white">회원 관리</h1>
       <div className="mb-10 mt-20 flex gap-5 ">
         {["회원", "지원자"].map((label) => (
           <p
@@ -15,13 +16,14 @@ const MemberManage = () => {
             className={`font-bold text-xl cursor-pointer relative pb-2 ${
               memberTab === label
                 ? "after:content-['']  text-white after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-white"
-                : "text-[#ccc]"
+                : "text-gray300"
             }`}
           >
             {label}
           </p>
         ))}
       </div>
+      <Member />
     </div>
   );
 };
