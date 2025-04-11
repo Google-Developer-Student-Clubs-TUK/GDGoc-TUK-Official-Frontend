d5f85782975ca3562c5f30c75c67054b1204834e
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2025-04-12 00:58:23 +0900

    feat : 회원관리 페이지 공통  컴포넌트 제작

---
+++ src/app/(route)/memberManage/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState } from "react";
+
+const MemberManage = () => {
+  const [memberTab, setMemberTap] = useState<"회원" | "지원자">("회원");
+
+  return (
+    <div className="my-[200px] mx-[100px] flex flex-col">
+      <h1 className="text-[28px] font-bold text-white">회원 관리</h1>
+      <div className="mb-10 mt-20 flex gap-5 ">
+        {["회원", "지원자"].map((label) => (
+          <p
+            key={label}
+            onClick={() => setMemberTap(label as "회원" | "지원자")}
+            className={`font-bold text-xl cursor-pointer relative pb-2 ${
+              memberTab === label
+                ? "after:content-['']  text-white after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-white"
+                : "text-[#ccc]"
+            }`}
+          >
+            {label}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MemberManage;
