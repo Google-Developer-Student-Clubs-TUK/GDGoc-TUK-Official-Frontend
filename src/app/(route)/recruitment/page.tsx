b3b18a6487170e19f360a1a7f3deedecffe462cc
Kim Hee Chan <a01049984394k@gmail.com>
2025-04-17 00:59:48 +0900

    feat: 지원하기 선택범위 수정

--- src/app/(route)/recruitment/page.tsx
+++ src/app/(route)/recruitment/page.tsx
@@ -40,7 +40,7 @@ const Recruitment = () => {
       <div className="relative w-1/2 flex flex-col justify-center items-center bg-[url('/image/recruitment-bg.png')] bg-cover bg-center group">
         <div id="overlay" className="absolute z-0 top-0 left-0 h-full w-full bg-gradient-to-r from-[#2E2E2E] to-[#2E2E2E50] cursor-pointer"></div>
 
-        <div className="z-10 relative overflow-hidden">
+        <div className="z-10 relative overflow-hidden cursor-pointer">
           <div className="absolute inset-0 bg-primary origin-left scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out" />
 
           <div className="relative z-10 flex items-center gap-2 text-white px-2 py-1">
