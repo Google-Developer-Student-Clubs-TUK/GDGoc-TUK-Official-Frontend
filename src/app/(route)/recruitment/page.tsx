452a76c7b40193a3c6b6d11f81349a9fe35e5a05
Kim Hee Chan <a01049984394k@gmail.com>
2025-04-21 15:37:29 +0900

    feat: 지원 불가능 컴포넌트 추가

--- src/app/(route)/recruitment/page.tsx
+++ src/app/(route)/recruitment/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import Image from "next/image";
+
+import AvailableApply from "./components/AvailableApply";
+import UnAvailableApply from "./components/UnAvailableApply";
 
 const Recruitment = () => {
+
+  const isAvailable = true; // 지원 가능 여부.
 
   return (
     <div className="flex pl-[100px] h-screen">
@@ -37,18 +41,8 @@ const Recruitment = () => {
       </div>
 
       {/* Right */}
-      <div className="relative w-1/2 flex flex-col justify-center items-center bg-[url('/image/recruitment-bg.png')] bg-cover bg-center group">
-        <div id="overlay" className="absolute z-0 top-0 left-0 h-full w-full bg-gradient-to-r from-[#2E2E2E] to-[#2E2E2E50] cursor-pointer"></div>
+      {isAvailable ? <AvailableApply /> : <UnAvailableApply />}
 
-        <div className="z-10 relative overflow-hidden cursor-pointer">
-          <div className="absolute inset-0 bg-primary origin-left scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out" />
-
-          <div className="relative z-10 flex items-center gap-2 text-white px-2 py-1">
-            <p className="text-ttMd font-bold">지원하기</p>
-            <Image src="/icon/slideup_arrow.svg" alt="화살표" width={20} height={20} className="object-contain rotate-90 h-fit"/>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
