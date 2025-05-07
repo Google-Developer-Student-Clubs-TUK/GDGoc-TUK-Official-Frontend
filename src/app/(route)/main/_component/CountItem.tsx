be56f063e060bd40f58b471f828c54bae832ce46
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2025-05-07 18:23:07 +0900

    feat: useCountUp 훅 추가

--- src/app/(route)/main/_component/CountItem.tsx
+++ src/app/(route)/main/_component/CountItem.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useCountUp } from "../_hook/useCountUp";
+import { useInView } from "react-intersection-observer";
 
-interface CountItmeProps {
+interface CountItemProps {
   title: string;
   count: number;
 }
-const CountItem = ({ title, count }: CountItmeProps) => {
+
+const CountItem = ({ title, count }: CountItemProps) => {
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.5 });
+  const countUp = useCountUp({ target: count, start: inView });
+
   return (
-    <div className="flex  text-white flex-col gap-2 w-[285px] items-center">
+    <div
+      ref={ref}
+      className="flex text-white flex-col gap-2 w-[285px] items-center"
+    >
       <p className="text-base">{title}</p>
-      <p className=" text-6xl font-bold">{count}</p>
+      <p className="text-6xl font-bold">{countUp}</p>
     </div>
   );
 };
