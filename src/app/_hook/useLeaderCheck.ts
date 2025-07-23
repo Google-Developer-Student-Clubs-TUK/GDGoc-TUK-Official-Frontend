de0d893ffb374713f85819ad29d2bdc65e3385b7
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2025-07-23 22:00:31 +0900

    feat : 로그인 만료 확인 추가

--- src/app/_hook/useLeaderCheck.ts
+++ src/app/_hook/useLeaderCheck.ts
@@ -1,9 +1,12 @@
-// hooks/useLeaderCheck.ts
+'use client'
+
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { membersCheckApi } from "../_lib/_api";
+import { useRouter } from "next/navigation"; 
 
 export function useLeaderCheck() {
+  const router = useRouter();
   const [isLeader, setIsLeader] = useState(false);
   const [isLogin , setIsLogin] = useState(false)
 
@@ -13,20 +16,31 @@ export function useLeaderCheck() {
     queryFn: () => membersCheckApi(),
   });
   
-
 useEffect(() => {
-  console.log("로그인체크")
+  console.log("로그인체크");
+
   if (error) {
+    const name = localStorage.getItem("name");
+    
+    // 로그인 되어 있었던 흔적이 있으면, 만료로 간주
+    if (name) {
+      localStorage.removeItem("name");
+      localStorage.removeItem("role");
+      alert("로그인이 만료되었습니다");
+      router.push("/login");
+    }
+
+
     setIsLogin(false);
+    setIsLeader(false);
   }
 
   if (data) {
     setIsLogin(true);
-    if (data.data.role === "ROLE_LEADER") {
-      setIsLeader(true);
-    }
+    setIsLeader(data.data.role === "ROLE_LEADER");
   }
 }, [data, error]);
+
 
 
   return {isLogin,isLeader};
